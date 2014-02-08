174ee29431124d68d298f0163e40f0f21f99fa1d
Vida Ha <vida@squareup.com>
2014-02-08 13:53:04 -0800

    Create the initial ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wecode::Application.config.secret_key_base = '2162fa647a7929caac65a1e4b3267f8f37aef409cc425123ed921a54283a99bfa376ea759cf6bbf760d271b43f0a5705d167d8135897510300f4ddb538bc68ec'
