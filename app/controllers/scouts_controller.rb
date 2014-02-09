6463bfea2b1a00cf94dc43b9cfd9acc24ff8e6cf
Vida Ha <vida@squareup.com>
2014-02-09 09:21:43 -0800

    Add the leaderboard page

---
+++ app/controllers/scouts_controller.rb
@@ -0,0 +1,6 @@
+class ScoutsController < ApplicationController
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wecode::Application.routes.draw do
+  root 'scouts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
